03f4d594d296f2440fbc9be231eda1e72617228a
ghost9394 <113897033+ghost9394@users.noreply.github.com>
2025-08-12 16:48:59 +0800

    Update ModelOrder.kt

--- app/src/main/java/fansirsqi/xposed/sesame/model/ModelOrder.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/model/ModelOrder.kt
@@ -9,13 +9,14 @@ import fansirsqi.xposed.sesame.task.antForest.AntForest
 import fansirsqi.xposed.sesame.task.antOcean.AntOcean
 import fansirsqi.xposed.sesame.task.antSports.AntSports
 import fansirsqi.xposed.sesame.task.reserve.Reserve
+import fansirsqi.xposed.sesame.task.antStall.AntStall
 
 object ModelOrder {
     private val array = arrayOf(
         BaseModel::class.java,       // 基础设置
         AntForest::class.java,       // 森林
         AntFarm::class.java,         // 庄园
-//        AntOrchard::class.java,    // 农场
+        AntOrchard::class.java,    // 农场
         AntOcean::class.java,        // 海洋
         AntDodo::class.java,       // 神奇物种
         AncientTree::class.java,     // 古树
@@ -23,7 +24,7 @@ object ModelOrder {
         Reserve::class.java,       // 保护地
         AntSports::class.java,       // 运动
 //        AntMember::class.java,     // 会员
-//        AntStall::class.java,      // 蚂蚁新村
+        AntStall::class.java,      // 蚂蚁新村
 //        GreenFinance::class.java,  // 绿色经营
 //        AntBookRead::class.java,   // 读书
 //        ConsumeGold::class.java,   // 消费金
